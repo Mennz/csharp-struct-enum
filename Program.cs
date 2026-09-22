4a541a4700485d97645a211f3c54bed72d8082c3
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-22 11:09:49 -0300

    arruma o WriteLine do dia pra ficar igual aos outros

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ Console.WriteLine("estudando struct e enum em C#");
 
 // enum comeca em 0, entao Domingo = 0
 DiaDaSemana hoje = DiaDaSemana.Terca;
-Console.WriteLine("hoje e " + hoje);
+Console.WriteLine($"hoje e {hoje}");
 
 Ponto p1 = new Ponto(0, 0);
 Ponto p2 = new Ponto(3, 4);
