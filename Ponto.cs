f375e8ab346777c0a64f72b695bf867e1776248b
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-22 11:08:32 -0300

    cria o struct Ponto

---
+++ Ponto.cs
@@ -0,0 +1,11 @@
+struct Ponto
+{
+    public double X;
+    public double Y;
+
+    public Ponto(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("estudando struct e enum em C#");
 DiaDaSemana hoje = DiaDaSemana.Terca;
 Console.WriteLine("hoje e " + hoje);
 
+Ponto p1 = new Ponto(0, 0);
+Ponto p2 = new Ponto(3, 4);
+Console.WriteLine($"p1 = ({p1.X}, {p1.Y})");
+Console.WriteLine($"p2 = ({p2.X}, {p2.Y})");
+
