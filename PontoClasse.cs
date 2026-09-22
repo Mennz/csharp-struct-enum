11ab2416887d82a0b19b548d211622ab015fb508
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-22 11:09:26 -0300

    compara struct e classe pra ver valor vs referencia

---
+++ PontoClasse.cs
@@ -0,0 +1,11 @@
+class PontoClasse
+{
+    public double X;
+    public double Y;
+
+    public PontoClasse(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,3 +10,15 @@ Console.WriteLine($"p1 = ({p1.X}, {p1.Y})");
 Console.WriteLine($"p2 = ({p2.X}, {p2.Y})");
 Console.WriteLine($"distancia entre p1 e p2 = {p1.DistanciaAte(p2)}");
 
+Console.WriteLine();
+Console.WriteLine("agora comparando struct com classe");
+
+Ponto copiaStruct = p1;
+copiaStruct.X = 99;
+Console.WriteLine($"p1 (struct) continua ({p1.X}, {p1.Y}), a copia mudou pra ({copiaStruct.X}, {copiaStruct.Y})");
+
+PontoClasse original = new PontoClasse(0, 0);
+PontoClasse copiaClasse = original;
+copiaClasse.X = 99;
+Console.WriteLine($"original (classe) virou ({original.X}, {original.Y}) porque copiaClasse aponta pro mesmo objeto");
+
